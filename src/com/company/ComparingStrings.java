e442bd0e3bb146344bcd29ac3ca01b1fea634684
Someone else <random@email.com>
2018-09-24 15:40:28 -0400

    Initial commit

---
+++ src/com/company/ComparingStrings.java
@@ -0,0 +1,28 @@
+package com.company;
+
+
+import java.util.Scanner;
+
+public class ComparingStrings {
+
+    public static void main(String []args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("How is the weather?");
+        String answer=sc.nextLine();
+
+        if (answer.equals("rain"))  //.equals() vs ==
+            System.out.println("Take your umbrella!");
+        else if (answer.equals("windy"))
+            System.out.println("Wear your jacket!");
+        else if (answer.equals("snow"))
+            System.out.println("You should stay inside!");
+        else if (answer.equals("stormy"))
+            System.out.println("Wear a coat and take a shovel!");
+        else
+            System.out.println("Enjoy your day!");
+    }
+
+}
+
